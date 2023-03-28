8a5a550e6869966b444b4991937ba416dc4e6ca3
Travis Frisinger <tmfrisinger@gmail.com>
2023-03-28 17:08:36 -0600

    better working version of test.js for video composing

--- src/3. video-director/test.js
+++ src/3. video-director/test.js
@@ -13,82 +13,85 @@ const inboxDir = "../../inbox";
 // ------- MERGE AUDIO ------------
 
 const fullAudioFile = `${inboxDir}/test/full-audio.wav`;
-// fs.unlinkSync(fullAudioFile);
+fs.unlinkSync(fullAudioFile);
 
 const audioCommand = ffmpeg();
 audioCommand.mergeAdd(`${inboxDir}/test/audio/000-0-000-story.wav`)
             .mergeAdd(`${inboxDir}/test/audio/0000-fullText.wav`)
-            .mergeToFile(fullAudioFile);
+            .mergeToFile(fullAudioFile).on('end', ()=>{
+                // -------- SET TEXT AND ADD AUDIO TRACK ---------
+                const command = ffmpeg(`${inboxDir}/test/02-snowboard.mp4`);
+                command.videoFilters([{
+                filter: 'drawtext',
+                options: {
+                    fontfile:'/vagrant/fonts/LucidaGrande.ttc',
+                    text: 'Story 1',
+                    fontsize: 72,
+                    fontcolor: 'white',
+                    x: '(main_w/2-text_w/2)',
+                    y: 50,
+                    shadowcolor: 'black',
+                    shadowx: 2,
+                    shadowy: 2,
+                    box:1,
+                    boxcolor:'blue@0.75',
+                    boxborderw:15,
+                    enable:'between(t,0,1.4)'
+                }
+                },{
+                    filter: 'drawtext',
+                    options: {
+                    fontfile:'/vagrant/fonts/LucidaGrande.ttc',
+                    text: 'I’m a casino dealer.\nPeople losing money brings out',
+                    fontsize: 90,
+                    fontcolor: 'white',
+                    x: '(main_w/2-text_w/2)',
+                    y: '((main_h-text_h)/2)',
+                    shadowcolor: 'black',
+                    shadowx: 2,
+                    shadowy: 2,
+                    enable:'between(t,1.5,5.1)'
+                    }
+                },{
+                    filter: 'drawtext',
+                    options: {
+                    fontfile:'/vagrant/fonts/LucidaGrande.ttc',
+                    text: 'the worst qualities in them.\nEspecially when I',
+                    fontsize: 90,
+                    fontcolor: 'white',
+                    x: '(main_w/2-text_w/2)',
+                    y: '((main_h-text_h)/2)',
+                    shadowcolor: 'black',
+                    shadowx: 2,
+                    shadowy: 2,
+                    enable:'between(t,5.2,8.3)'
+                    }
+                },
+                {
+                    filter: 'drawtext',
+                    options: {
+                    fontfile:'/vagrant/fonts/LucidaGrande.ttc',
+                    text: 'deal high limit games. Plus the pit',
+                    fontsize: 90,
+                    fontcolor: 'white',
+                    x: '(main_w/2-text_w/2)',
+                    y: '((main_h-text_h)/2)',
+                    shadowcolor: 'black',
+                    shadowx: 2,
+                    shadowy: 2,
+                    enable:'between(t,8.4,11.2)'
+                    }
+                }])
+                .addInput(`${inboxDir}/test/full-audio.wav`)
+                .saveToFile(`${inboxDir}/test/composed-test.mp4`);
+            });
+
 
 
 // todo : cobine audio into a single file, the add as source and set text filters to draw on
 // can pass a list of filters to videoFilters
 
-// -------- SET TEXT AND ADD AUDIO ---------
-const command = ffmpeg(`${inboxDir}/test/02-snowboard.mp4`);
-command.videoFilters([{
-  filter: 'drawtext',
-  options: {
-    fontfile:'/vagrant/fonts/LucidaGrande.ttc',
-    text: 'Story 1',
-    fontsize: 72,
-    fontcolor: 'white',
-    x: '(main_w/2-text_w/2)',
-    y: 50,
-    shadowcolor: 'black',
-    shadowx: 2,
-    shadowy: 2,
-    box:1,
-    boxcolor:'blue@0.75',
-    boxborderw:15,
-    enable:'between(t,0,1.4)'
-  }
-},{
-    filter: 'drawtext',
-    options: {
-      fontfile:'/vagrant/fonts/LucidaGrande.ttc',
-      text: 'I’m a casino dealer.\nPeople losing money brings out',
-      fontsize: 90,
-      fontcolor: 'white',
-      x: '(main_w/2-text_w/2)',
-      y: '((main_h-text_h)/2)',
-      shadowcolor: 'black',
-      shadowx: 2,
-      shadowy: 2,
-      enable:'between(t,1.5,5.1)'
-    }
-  },{
-    filter: 'drawtext',
-    options: {
-      fontfile:'/vagrant/fonts/LucidaGrande.ttc',
-      text: 'the worst qualities in them.\nEspecially when I',
-      fontsize: 90,
-      fontcolor: 'white',
-      x: '(main_w/2-text_w/2)',
-      y: '((main_h-text_h)/2)',
-      shadowcolor: 'black',
-      shadowx: 2,
-      shadowy: 2,
-      enable:'between(t,5.2,8.3)'
-    }
-  },
-  {
-    filter: 'drawtext',
-    options: {
-      fontfile:'/vagrant/fonts/LucidaGrande.ttc',
-      text: 'deal high limit games. Plus the pit',
-      fontsize: 90,
-      fontcolor: 'white',
-      x: '(main_w/2-text_w/2)',
-      y: '((main_h-text_h)/2)',
-      shadowcolor: 'black',
-      shadowx: 2,
-      shadowy: 2,
-      enable:'between(t,8.4,11.2)'
-    }
-  }])
-  .addInput(`${inboxDir}/test/full-audio.wav`)
-.saveToFile(`${inboxDir}/test/composed-test.mp4`);
+
 
 
 // story text
