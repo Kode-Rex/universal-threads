6713ad955722afc3bf93d02f8eaf7e5f145024ca
Travis Frisinger <tmfrisinger@gmail.com>
2023-03-28 16:28:21 -0600

    got a simple sample working

--- src/3. video-director/test.js
+++ src/3. video-director/test.js
@@ -6,46 +6,84 @@ const inboxDir = "../../inbox";
 
 // todo : seed all the fun stuff here to get a working sample 
 
-const command = ffmpeg(`${inboxDir}/test/02-snowboard.mp4`);
+
 
 // audio addition ?
 // https://stackoverflow.com/questions/66183650/how-to-add-multiple-audio-to-a-video-at-specific-time-for-specific-duration
-// -----------------------------------------
+// ------- MERGE AUDIO ------------
+const audioCommand = ffmpeg();
+audioCommand.mergeAdd(`${inboxDir}/test/audio/000-0-000-story.wav`)
+            .mergeAdd(`${inboxDir}/test/audio/0000-fullText.wav`)
+            .mergeToFile(`${inboxDir}/test/full-audio.wav`);
 
-// story title - 
-//     enable:between(t,1,5)
-command.videoFilters({
+
+// todo : cobine audio into a single file, the add as source and set text filters to draw on
+// can pass a list of filters to videoFilters
+
+// -------- SET TEXT AND ADD AUDIO ---------
+const command = ffmpeg(`${inboxDir}/test/02-snowboard.mp4`);
+command.videoFilters([{
   filter: 'drawtext',
   options: {
     fontfile:'/vagrant/fonts/LucidaGrande.ttc',
     text: 'Story 1',
     fontsize: 72,
-    fontcolor: 'black',
+    fontcolor: 'white',
     x: '(main_w/2-text_w/2)',
     y: 50,
-    shadowcolor: 'white',
+    shadowcolor: 'black',
     shadowx: 2,
     shadowy: 2,
     box:1,
     boxcolor:'blue@0.75',
     boxborderw:15,
-    enable:'between(t,0,5)'
+    enable:'between(t,0.1,1.4)'
   }
-}).videoFilters({
+},{
     filter: 'drawtext',
     options: {
       fontfile:'/vagrant/fonts/LucidaGrande.ttc',
-      text: 'THIS IS TEXT THAT I AM TESTING WITH',
-      fontsize: 72,
+      text: 'I’m a casino dealer.\nPeople losing money brings out',
+      fontsize: 90,
+      fontcolor: 'white',
+      x: '(main_w/2-text_w/2)',
+      y: '((main_h-text_h)/2)',
+      shadowcolor: 'black',
+      shadowx: 2,
+      shadowy: 2,
+      enable:'between(t,1.5,5.1)'
+    }
+  },{
+    filter: 'drawtext',
+    options: {
+      fontfile:'/vagrant/fonts/LucidaGrande.ttc',
+      text: 'the worst qualities in them.\nEspecially when I',
+      fontsize: 90,
       fontcolor: 'blue',
       x: '(main_w/2-text_w/2)',
       y: '((main_h-text_h)/2)',
       shadowcolor: 'black',
       shadowx: 2,
       shadowy: 2,
-      enable:'between(t,5,20)'
+      enable:'between(t,5.2,8.5)'
     }
-  })
+  },
+  {
+    filter: 'drawtext',
+    options: {
+      fontfile:'/vagrant/fonts/LucidaGrande.ttc',
+      text: 'deal high limit games. Plus the pit',
+      fontsize: 90,
+      fontcolor: 'blue',
+      x: '(main_w/2-text_w/2)',
+      y: '((main_h-text_h)/2)',
+      shadowcolor: 'black',
+      shadowx: 2,
+      shadowy: 2,
+      enable:'between(t,8.6,11.5)'
+    }
+  }])
+  .addInput(`${inboxDir}/test/full-audio.wav`)
 .saveToFile(`${inboxDir}/test/composed-test.mp4`);
 
 
