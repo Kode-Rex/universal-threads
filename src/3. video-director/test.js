dbab6298efe80694aa624a9558ca094b609f5563
Travis Frisinger <tmfrisinger@gmail.com>
2023-03-28 21:57:34 -0600

    start of dynamic text filters

--- src/3. video-director/test.js
+++ src/3. video-director/test.js
@@ -9,83 +9,252 @@ const fontPath = `${inboxDir}/test/BebasNeue-Regular.ttf`
 // ------- MERGE AUDIO ------------
 const context = JSON.parse(fs.readFileSync(`${inboxDir}/test/context.json`));
 
+const textFilters = [];
+let filterStart = context.duration / 1000; // allow for image to show in intro
 // title, then each story's full text
 const audioCommand = ffmpeg();
 const fullAudioFile = `${inboxDir}/test/full-audio.wav`;
 audioCommand.mergeAdd(context.filePath);
 context.stories.forEach((val, idx)=>{
     audioCommand.mergeAdd(val.filePath); // story #
+
+    // pushing story # text
+    let betweenText = `between(t,${filterStart},${filterStart+(val.duration/1000)})`
+    textFilters.push({
+        filter: 'drawtext',
+        options: {
+                fontfile: fontPath,
+                text: `Story ${val.seq+1}`,
+                fontsize: 72,
+                fontcolor: 'white',
+                x: '(main_w/2-text_w/2)',
+                y: 50,
+                shadowcolor: 'black',
+                shadowx: 2,
+                shadowy: 2,
+                box:1,
+                boxcolor:'blue@0.75',
+                boxborderw:10,
+                enable:betweenText
+            }
+        });
+    
+    filterStart += ((val.duration/1000)); // 10 ms added to buffer start-stops
     audioCommand.mergeAdd(val.fullTextPath); // story text
+    // val.ttsSegments.forEach((tts, idx)=>{
+    //     betweenText = `between(t,${filterStart},${filterStart+(val.duration)})`
+    //     textFilters.push({
+    //         filter: 'drawtext',
+    //         options: {
+    //                 fontfile: fontPath,
+    //                 text: tts.displayTest,
+    //                 fontsize: 90,
+    //                 fontcolor: 'white',
+    //                 x: '(main_w/2-text_w/2)',
+    //                 y: '((main_h-text_h)/2)',
+    //                 shadowcolor: 'black',
+    //                 shadowx: 2,
+    //                 shadowy: 2,
+    //                 box:1,
+    //                 enable:betweenText
+    //             }
+    //     });
+    //     filterStart += tts.duration+100; // 10 ms added to buffer start-stops
+    // });
+
+    
+    // todo  I almost want to build up the filters array too?
 });
+
+console.log(JSON.stringify(textFilters));
 
 audioCommand.mergeToFile(fullAudioFile).on('end',()=>{
     // process the text
      // -------- SET TEXT AND ADD AUDIO TRACK ---------
                 // todo : write options to file so they can be tweaked ?
-                const command = ffmpeg(`${inboxDir}/test/06-pro-shredder.mp4`);
-                command.videoFilters([{
-                filter: 'drawtext',
-                options: {
-                    fontfile: fontPath,
-                    text: 'Story 1',
-                    fontsize: 72,
-                    fontcolor: 'white',
-                    x: '(main_w/2-text_w/2)',
-                    y: 50,
-                    shadowcolor: 'black',
-                    shadowx: 2,
-                    shadowy: 2,
-                    box:1,
-                    boxcolor:'blue@0.75',
-                    boxborderw:10,
-                    enable:'between(t,0,1.4)'
-                }
-                },{
-                    filter: 'drawtext',
-                    options: {
-                    fontfile:fontPath,
-                    text: 'I’m a casino dealer.\nPeople losing money brings out',
-                    fontsize: 90,
-                    fontcolor: 'white',
-                    x: '(main_w/2-text_w/2)',
-                    y: '((main_h-text_h)/2)',
-                    shadowcolor: 'black',
-                    shadowx: 2,
-                    shadowy: 2,
-                    enable:'between(t,1.5,5.1)'
-                    }
-                },{
-                    filter: 'drawtext',
-                    options: {
-                    fontfile:fontPath,
-                    text: 'the worst qualities in them.\nEspecially when I',
-                    fontsize: 90,
-                    fontcolor: 'white',
-                    x: '(main_w/2-text_w/2)',
-                    y: '((main_h-text_h)/2)',
-                    shadowcolor: 'black',
-                    shadowx: 2,
-                    shadowy: 2,
-                    enable:'between(t,5.2,8.3)'
-                    }
-                },
-                {
-                    filter: 'drawtext',
-                    options: {
-                    fontfile:fontPath,
-                    text: 'deal high limit games. Plus the pit',
-                    fontsize: 90,
-                    fontcolor: 'white',
-                    x: '(main_w/2-text_w/2)',
-                    y: '((main_h-text_h)/2)',
-                    shadowcolor: 'black',
-                    shadowx: 2,
-                    shadowy: 2,
-                    enable:'between(t,8.4,11.2)'
-                    }
-                }])
+                const command = ffmpeg();
+                command.input(`${inboxDir}/test/06-pro-shredder.mp4`); //.input(`${inboxDir}/test/thread.png`);
+
+                //command.videoFilters(textFilters)
+                command.videoFilters(
+                    [
+                        {
+                            "filter": "drawtext",
+                            "options": {
+                                "fontfile": "../../inbox/test/BebasNeue-Regular.ttf",
+                                "text": "Story 1",
+                                "fontsize": 72,
+                                "fontcolor": "white",
+                                "x": "(main_w/2-text_w/2)",
+                                "y": 50,
+                                "shadowcolor": "black",
+                                "shadowx": 2,
+                                "shadowy": 2,
+                                "box": 1,
+                                "boxcolor": "blue@0.75",
+                                "boxborderw": 10,
+                                "enable": "between(t,4.688,6.090999999999999)"
+                            }
+                        },
+                        {
+                            "filter": "drawtext",
+                            "options": {
+                                "fontfile": "../../inbox/test/BebasNeue-Regular.ttf",
+                                "text": "Story 2",
+                                "fontsize": 72,
+                                "fontcolor": "white",
+                                "x": "(main_w/2-text_w/2)",
+                                "y": 50,
+                                "shadowcolor": "black",
+                                "shadowx": 2,
+                                "shadowy": 2,
+                                "box": 1,
+                                "boxcolor": "blue@0.75",
+                                "boxborderw": 10,
+                                "enable": "between(t,6.090999999999999,7.507999999999999)"
+                            }
+                        },
+                        {
+                            "filter": "drawtext",
+                            "options": {
+                                "fontfile": "../../inbox/test/BebasNeue-Regular.ttf",
+                                "text": "Story 3",
+                                "fontsize": 72,
+                                "fontcolor": "white",
+                                "x": "(main_w/2-text_w/2)",
+                                "y": 50,
+                                "shadowcolor": "black",
+                                "shadowx": 2,
+                                "shadowy": 2,
+                                "box": 1,
+                                "boxcolor": "blue@0.75",
+                                "boxborderw": 10,
+                                "enable": "between(t,7.507999999999999,8.956)"
+                            }
+                        },
+                        {
+                            "filter": "drawtext",
+                            "options": {
+                                "fontfile": "../../inbox/test/BebasNeue-Regular.ttf",
+                                "text": "Story 4",
+                                "fontsize": 72,
+                                "fontcolor": "white",
+                                "x": "(main_w/2-text_w/2)",
+                                "y": 50,
+                                "shadowcolor": "black",
+                                "shadowx": 2,
+                                "shadowy": 2,
+                                "box": 1,
+                                "boxcolor": "blue@0.75",
+                                "boxborderw": 10,
+                                "enable": "between(t,8.956,10.405999999999999)"
+                            }
+                        },
+                        {
+                            "filter": "drawtext",
+                            "options": {
+                                "fontfile": "../../inbox/test/BebasNeue-Regular.ttf",
+                                "text": "Story 5",
+                                "fontsize": 72,
+                                "fontcolor": "white",
+                                "x": "(main_w/2-text_w/2)",
+                                "y": 50,
+                                "shadowcolor": "black",
+                                "shadowx": 2,
+                                "shadowy": 2,
+                                "box": 1,
+                                "boxcolor": "blue@0.75",
+                                "boxborderw": 10,
+                                "enable": "between(t,10.405999999999999,11.884999999999998)"
+                            }
+                        },
+                        {
+                            "filter": "drawtext",
+                            "options": {
+                                "fontfile": "../../inbox/test/BebasNeue-Regular.ttf",
+                                "text": "Story 6",
+                                "fontsize": 72,
+                                "fontcolor": "white",
+                                "x": "(main_w/2-text_w/2)",
+                                "y": 50,
+                                "shadowcolor": "black",
+                                "shadowx": 2,
+                                "shadowy": 2,
+                                "box": 1,
+                                "boxcolor": "blue@0.75",
+                                "boxborderw": 10,
+                                "enable": "between(t,11.884999999999998,13.473999999999998)"
+                            }
+                        }
+                    ]
+                )
                 .addInput(`${inboxDir}/test/full-audio.wav`)
                 .saveToFile(`${inboxDir}/test/composed-test.mp4`);
+                // tell it to overlay the image 
+                //command.complexFilter(["[0:v]scale=640:-1[bg];[bg][1:v]overlay=W-w-10:H-h-10"]);
+
+                // command.videoFilters([{
+                // filter: 'drawtext',
+                // options: {
+                //     fontfile: fontPath,
+                //     text: 'Story 1',
+                //     fontsize: 72,
+                //     fontcolor: 'white',
+                //     x: '(main_w/2-text_w/2)',
+                //     y: 50,
+                //     shadowcolor: 'black',
+                //     shadowx: 2,
+                //     shadowy: 2,
+                //     box:1,
+                //     boxcolor:'blue@0.75',
+                //     boxborderw:10,
+                //     enable:'between(t,0,1.4)'
+                // }
+                // },{
+                //     filter: 'drawtext',
+                //     options: {
+                //     fontfile:fontPath,
+                //     text: 'I’m a casino dealer.\nPeople losing money brings out',
+                //     fontsize: 90,
+                //     fontcolor: 'white',
+                //     x: '(main_w/2-text_w/2)',
+                //     y: '((main_h-text_h)/2)',
+                //     shadowcolor: 'black',
+                //     shadowx: 2,
+                //     shadowy: 2,
+                //     enable:'between(t,1.5,5.1)'
+                //     }
+                // },{
+                //     filter: 'drawtext',
+                //     options: {
+                //     fontfile:fontPath,
+                //     text: 'the worst qualities in them.\nEspecially when I',
+                //     fontsize: 90,
+                //     fontcolor: 'white',
+                //     x: '(main_w/2-text_w/2)',
+                //     y: '((main_h-text_h)/2)',
+                //     shadowcolor: 'black',
+                //     shadowx: 2,
+                //     shadowy: 2,
+                //     enable:'between(t,5.2,8.3)'
+                //     }
+                // },
+                // {
+                //     filter: 'drawtext',
+                //     options: {
+                //     fontfile:fontPath,
+                //     text: 'deal high limit games. Plus the pit',
+                //     fontsize: 90,
+                //     fontcolor: 'white',
+                //     x: '(main_w/2-text_w/2)',
+                //     y: '((main_h-text_h)/2)',
+                //     shadowcolor: 'black',
+                //     shadowx: 2,
+                //     shadowy: 2,
+                //     enable:'between(t,8.4,11.2)'
+                //     }
+                // }])
+                
 });
 
 // todo : cobine audio into a single file, the add as source and set text filters to draw on
