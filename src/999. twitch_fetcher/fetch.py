a2738c2a583c96462a23a4d174234689ea66b265
Travis Frisinger <tmfrisinger@gmail.com>
2023-10-08 15:18:44 -0600

    added in python script to start work on twitch video fetching

---
+++ src/999. twitch_fetcher/fetch.py
@@ -0,0 +1,83 @@
+import requests
+import cv2
+import os
+
+TWITCH_CLIENT_ID = 'YOUR_TWITCH_CLIENT_ID'
+TWITCH_OAUTH_TOKEN = 'YOUR_TWITCH_OAUTH_TOKEN'
+HEADERS = {
+    'Client-ID': TWITCH_CLIENT_ID,
+    'Authorization': f"Bearer {TWITCH_OAUTH_TOKEN}"
+}
+
+def get_game_ids(game_names):
+    game_ids = []
+    
+    for game_name in game_names:
+        response = requests.get('https://api.twitch.tv/helix/games', headers=HEADERS, params={'name': game_name})
+        data = response.json()
+        
+        if data['data']:
+            game_ids.append(data['data'][0]['id'])
+
+    return game_ids
+
+def fetch_random_twitch_clips_for_games(game_ids):
+    clips = []
+    
+    for game_id in game_ids:
+        params = {
+            'game_id': game_id,
+            'first': 100,
+        }
+        response = requests.get('https://api.twitch.tv/helix/clips', headers=HEADERS, params=params)
+        data = response.json()
+        clips.extend(data['data'])
+
+    return clips
+
+def contains_face(video_path):
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    cap = cv2.VideoCapture(video_path)
+
+    while True:
+        ret, frame = cap.read()
+        
+        if not ret:
+            break
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+        if len(faces) > 0:
+            cap.release()
+            return True
+
+    cap.release()
+    return False
+
+def download_clip(clip):
+    clip_url = clip['thumbnail_url'].split("-preview", 1)[0] + ".mp4"
+    response = requests.get(clip_url, stream=True)
+    video_path = f"{clip['id']}.mp4"
+    
+    with open(video_path, 'wb') as out_file:
+        for chunk in response.iter_content(chunk_size=8192):
+            out_file.write(chunk)
+    
+    return video_path
+
+if __name__ == "__main__":
+    game_names = ["Minecraft", "Animal Crossing", "Terraria", "Celeste", "Slime Rancher",
+                  "Stardew Valley", "Undertale", "Spiritfarer", 
+                  "Rocket League", "Abzu", "Firewatch", "Planet Coaster", 
+                  "Two Point Hospital", "Portal", "Portal 2", "Super Mario Odyssey"]
+    game_ids = get_game_ids(game_names)
+    clips = fetch_random_twitch_clips_for_games(game_ids)
+
+    for clip in clips:
+        video_path = download_clip(clip)
+        
+        if contains_face(video_path):
+            print(f"Faces detected in clip: {clip['id']}")
+            os.remove(video_path)  # Optionally, remove the video if it contains a face
+
